554843d5d42c0ed90a35bc522f9b6422babb5a6f
vicnaum <vicnaum@gmail.com>
2025-04-20 21:18:57 +0200

    fix: remove deprecated swcMinify option

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: "standalone", // Optimizes the build for production
   poweredByHeader: false, // Removes the X-Powered-By header
   reactStrictMode: true,
-  swcMinify: true, // Uses SWC for minification instead of Terser
 };
 
 export default nextConfig;
