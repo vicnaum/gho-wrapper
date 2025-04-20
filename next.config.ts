027450848423ac850065e24f231a253746674f38
vicnaum <vicnaum@gmail.com>
2025-04-20 21:11:11 +0200

    nixpacks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Optimizes the build for production
+  poweredByHeader: false, // Removes the X-Powered-By header
+  reactStrictMode: true,
+  swcMinify: true, // Uses SWC for minification instead of Terser
 };
 
 export default nextConfig;
